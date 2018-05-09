4312c11cd465585ad98a1a181a9b11e61d035048
Pavel Balusek <mr.p.balusek@gmail.com>
2018-05-09 14:01:22 +0200

    Editor changes. Started to create first GUI

--- src/Engine/EditorScene.java
+++ src/Engine/EditorScene.java
@@ -1,9 +1,13 @@
 package Engine;
 
+import Game.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,16 +15,24 @@ import java.util.ResourceBundle;
 public class Editor implements Initializable {
 
     @FXML
-    private Label label;
+    private Pane gameWindow;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Game basicGame = new Game();
+        try {
+            basicGame.createGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        GridPane game = basicGame.getGame();
+        gameWindow.getChildren().add(game);
+
     }
 }
